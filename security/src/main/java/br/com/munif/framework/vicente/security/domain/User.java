371077c4052b52f051a10b6f33ee27f7a3c1794e
Munif Gebara Junior <munif@MacBook-Pro-de-Munif.local>
2017-08-28 20:44:10 -0300

    security

---
+++ security/src/main/java/br/com/munif/framework/vicente/security/domain/User.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.munif.framework.vicente.security.domain;
+
+import br.com.munif.framework.vicente.domain.BaseEntity;
+
+/**
+ *
+ * @author munif
+ */
+public class User extends BaseEntity{
+    
+    private String login;
+    private String password;
+    private String group;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+    
+    
+}
