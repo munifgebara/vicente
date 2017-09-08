1c52eb206dbd8c4bb2832053c4e8896067f88c7b
munifgebara <munifgebara@gmail.com>
2017-09-08 04:06:14 -0300

    testes

---
+++ api/src/main/java/br/com/munif/framework/vicente/api/VicenteCreateWithExistingIdException.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.munif.framework.vicente.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ *
+ * @author munif
+ */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class VicenteCreateWithExistingIdException extends RuntimeException {
+
+    public VicenteCreateWithExistingIdException(String string) {
+        super(string);
+    }
+    
+}
