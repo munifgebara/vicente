c8d5b73768cf541f6577927389d05f6e3ab2d454
Munif Gebara Junior <munifgebara@gmail.com>
2018-02-15 04:58:15 -0200

    diagrama

---
+++ application/src/main/java/br/com/munif/framework/vicente/application/EntityDiagramBuilder.java
@@ -0,0 +1,28 @@
+package br.com.munif.framework.vicente.application;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Set;
+import org.reflections.Reflections;
+import javax.persistence.Entity;
+
+public class EntityDiagramBuilder {
+
+    public static void main(String args[]) throws IOException {
+        Reflections reflections = new Reflections();
+        Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(Entity.class);    
+        System.out.println("--->"+annotated);
+    }
+    
+    public List<Class> searchForEntities(File folder){
+        List<Class> toReturn=new ArrayList<Class>();
+        
+        return toReturn;
+    }
+
+}
--- core/src/main/java/br/com/munif/framework/vicente/core/Utils.java
+++ core/src/main/java/br/com/munif/framework/vicente/core/Utils.java
@@ -1,10 +1,14 @@
 package br.com.munif.framework.vicente.core;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -62,9 +66,51 @@ public class Utils {
 
     }
 
+    public static String windowsSafe(String s) {
+        return s.replaceAll("\\\\", "/");
+    }
+
     public static void main(String args[]) {
-        
+
         System.out.println(primeiraMinuscula("ABC"));
+
+    }
+
+    public static List<Class> scanClasses() throws IOException {
+        List<Class> toReturn = new ArrayList<>();
+        Enumeration<URL> resources = Thread.currentThread().getContextClassLoader().getResources("");
+        while (resources.hasMoreElements()) {
+            File file = new File(resources.nextElement().getFile());
+            toReturn.addAll(scanFolder(file, file));
+        }
+        return toReturn;
+    }
+
+    private static String getClassName(File f, File baseFolder) {
+        String compleName = Utils.windowsSafe(f.getAbsolutePath());
+        String baseFolderName = Utils.windowsSafe(baseFolder.getAbsolutePath());
+        return compleName.replaceFirst(baseFolderName, "").replace(".class", "").replaceAll("/", ".").replaceFirst(".", "");
+    }
+
+    public static List<Class> scanFolder(File folder, File baseFolder) {
+        List<Class> toReturn = new ArrayList<>();
+        File[] fs = folder.listFiles();
+        for (File f : fs) {
+            if (f.isDirectory()) {
+                toReturn.addAll(scanFolder(f, baseFolder));
+            } else {
+                String name = f.getName();
+                if (name.endsWith(".class")) {
+                    String className = getClassName(f, baseFolder);
+                    try {
+                        toReturn.add(Class.forName(className));
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        }
+        return toReturn;
 
     }
 
