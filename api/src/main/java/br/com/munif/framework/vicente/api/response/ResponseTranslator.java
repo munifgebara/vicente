e4bf81cf67ef0458169c2f070df4b6c34d1971be
willianmarquesfreire <willianmarquesfreire@gmail.com>
2022-10-15 14:49:44 -0300

    ad

--- api/src/main/java/br/com/munif/framework/vicente/api/response/ResponseTranslator.java
+++ api/src/main/java/br/com/munif/framework/vicente/api/response/ResponseTranslator.java
@@ -21,7 +21,7 @@ public class ResponseTranslator implements ResponseBodyAdvice<Object> {
 
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-        if (body instanceof VicError)
+        if (body instanceof VicError || body instanceof byte[])
             return body;
 
         HttpStatus status = HttpStatus.resolve(((ServletServerHttpResponse) response).getServletResponse().getStatus());
