afaa907bbc2a8e8cd3e60314183653025986d8ec
willianmarquesfreire <willianmarquesfreire@gmail.com>
2022-11-23 09:47:03 -0300

    ad

--- security/src/main/java/br/com/munif/framework/vicente/security/domain/exceptions/UserNotFoundException.java
+++ security/src/main/java/br/com/munif/framework/vicente/security/domain/exceptions/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package br.com.munif.framework.vicente.security.domain.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException() {
         super("User not found.");
