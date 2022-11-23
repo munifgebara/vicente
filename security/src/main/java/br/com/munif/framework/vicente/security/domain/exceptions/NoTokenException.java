028670259181eef42c368a29dbaba9d9ce2f3dfa
willianmarquesfreire <willianmarquesfreire@gmail.com>
2022-11-23 09:39:39 -0300

    ad

--- security/src/main/java/br/com/munif/framework/vicente/security/domain/exceptions/NoTokenException.java
+++ security/src/main/java/br/com/munif/framework/vicente/security/domain/exceptions/NoTokenException.java
@@ -1,5 +1,9 @@
 package br.com.munif.framework.vicente.security.domain.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class NoTokenException extends RuntimeException {
 
     public NoTokenException() {
--- security/src/main/java/br/com/munif/framework/vicente/security/domain/exceptions/TokenExpiredException.java
+++ security/src/main/java/br/com/munif/framework/vicente/security/domain/exceptions/TokenExpiredException.java
@@ -1,5 +1,9 @@
 package br.com.munif.framework.vicente.security.domain.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class TokenExpiredException extends RuntimeException {
 
     public TokenExpiredException() {
