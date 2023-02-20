b7c43ef93dd6f0a86ddd7002fdcc63d0b1329aed
willianmarquesfreire <willianmarquesfreire@gmail.com>
2023-02-20 17:10:51 -0300

    ad

--- security/src/main/java/br/com/munif/framework/vicente/security/domain/dto/ValidateEmailDto.java
+++ security/src/main/java/br/com/munif/framework/vicente/security/domain/dto/ValidateEmailDto.java
@@ -1,6 +1,10 @@
 package br.com.munif.framework.vicente.security.domain.dto;
 
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+
 public class ValidateEmailDto {
     public String email;
     public String code;
+    public ZonedDateTime expiration;
 }
